2a73c31acd7e31e6ed063919444c930eff0ec2df
= <rtyenriques@gmail.com>
2021-09-24 01:15:02 -0700

    commit tree 44a4feb932296e006b966537b9aa6afeb45ff7e1

---
+++ app/models/tag.rb
@@ -0,0 +1,10 @@
+class Tag < ApplicationRecord
+    belongs_to :user
+    has_many :post_tags
+    has_many :posts, through: :post_tags
+     
+  
+
+    validates :name, uniqueness: true
+    
+end
---
+++ db/migrate/20210924072946_add_tag_to_post_tag.rb
@@ -0,0 +1,5 @@
+class AddTagToPostTag < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :post_tags, :tag, foreign_key: true
+  end
+end
